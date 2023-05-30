53649d85033f52b32b6ff086e194abbc7dca03c0
Cynthia Chung <cynthiachung03@gmail.com>
2023-05-29 22:48:47 -0400

    Fix more ESLint problems

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -10,6 +10,7 @@ export const Button = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttrib
 
         return (
             <button
+                type="button"
                 className={twMerge(
                     "inline-flex w-full items-center justify-center rounded-md bg-primary p-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
                     className,
