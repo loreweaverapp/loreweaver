34d2d081f6327b733a3abafad89105a7db2bd06a
Lore Weaver <devops@loreweaver.app>
2023-05-29 23:01:36 -0400

    Staging -> Main (#3)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,39 +3,121 @@ const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
+    root: true,
+    extends: ["eslint:recommended", "airbnb-base"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
-      },
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+    plugins: ["@typescript-eslint"],
+    overrides: [
+        // Typescript-specific configuration
+        {
+            files: ["*.ts", "*.tsx"],
+            parserOptions: {
+                project: path.join(__dirname, "tsconfig.json"),
+            },
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@typescript-eslint/recommended-requiring-type-checking",
+                "plugin:import/typescript",
+                "airbnb-typescript/base",
+            ],
+            rules: {
+                "@typescript-eslint/ban-ts-comment": "off",
+                "@typescript-eslint/ban-types": "off",
+                "@typescript-eslint/no-empty-function": ["error", {allow: ["constructors"]}],
+                "@typescript-eslint/no-unsafe-assignment": "off",
+                "@typescript-eslint/no-unsafe-call": "off",
+                "@typescript-eslint/no-unsafe-member-access": "off",
+                "@typescript-eslint/no-unsafe-return": "off",
+                "@typescript-eslint/no-useless-constructor": "off",
+                "@typescript-eslint/unbound-method": "off",
+                "@typescript-eslint/no-explicit-any": "off",
+                "@typescript-eslint/no-unsafe-argument": "off",
+                "@typescript-eslint/consistent-type-imports": [
+                    "warn",
+                    {
+                        prefer: "type-imports",
+                        fixStyle: "inline-type-imports",
+                    },
+                ],
+                "@typescript-eslint/no-unused-vars": ["warn", {argsIgnorePattern: "^_"}],
+                "@typescript-eslint/no-misused-promises": [
+                    2,
+                    {
+                        checksVoidReturn: {attributes: false},
+                    },
+                ],
+            },
+            settings: {
+                "import/parsers": {
+                    "@typescript-eslint/parser": [".ts", ".tsx"],
+                },
+                "import/resolver": {
+                    typescript: {
+                        alwaysTryTypes: true,
+                        project: "tsconfig.json",
+                    },
+                },
+            },
+        },
+        // React-specific configuration
+        {
+            files: ["*.jsx", "*.tsx"],
+            extends: ["airbnb", "airbnb/hooks", "next/core-web-vitals"],
+            rules: {
+                // eslint-plugin-react
+                "react/destructuring-assignment": "off",
+                "react/jsx-props-no-spreading": "off",
+                "react/require-default-props": "off",
+                "react/react-in-jsx-scope": "off",
+            },
+        },
+        // React + Typescript-specific configuration
+        {
+            files: ["*.tsx"],
+            extends: ["airbnb-typescript"],
+        },
+        // Global override to ensure Prettier is the highest priority
+        {
+            files: ["*"],
+            extends: ["plugin:prettier/recommended"],
+            rules: {
+                // Built-in
+                "class-methods-use-this": "off",
+                "no-console": "off",
+                "no-empty-function": "off",
+                "no-nested-ternary": "off",
+                "no-param-reassign": "off",
+                "no-underscore-dangle": "off",
+                "no-void": "off",
+                // eslint-plugin-import
+                "import/extensions": "off",
+                "import/no-cycle": "off",
+                "import/no-extraneous-dependencies": [
+                    "error",
+                    {
+                        devDependencies: [
+                            "**/*.test.ts",
+                            "**/*.spec.ts",
+                            "**/*.int-spec.ts",
+                            "**/*.e2e-spec.ts",
+                            "**/jest.config.*",
+                            "**/next.config.js",
+                        ],
+                    },
+                ],
+                "import/prefer-default-export": "off",
+                "import/order": [
+                    "error",
+                    {
+                        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+                    },
+                ],
+            }
+        },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-misused-promises": [
-      2,
-      {
-        checksVoidReturn: { attributes: false },
-      },
-    ],
-  },
 };
 
 module.exports = config;
--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -1,18 +1,18 @@
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import {type inferRouterInputs, type inferRouterOutputs} from "@trpc/server";
 import superjson from "superjson";
-import { type AppRouter } from "$/server/api/root";
+import {type AppRouter} from "$/server/api/root";
 
 export const transformer = superjson;
 
 function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-  const vc = process.env.VERCEL_URL;
-  if (vc) return "https://" + vc;
-  return "http://localhost:3000";
+    if (typeof window !== "undefined") return "";
+    const vc = process.env.VERCEL_URL;
+    if (vc) return `https://${vc}`;
+    return "http://localhost:3000";
 }
 
 export function getUrl() {
-  return getBaseUrl() + "/api/trpc";
+    return `${getBaseUrl()}/api/trpc`;
 }
 
 /**
