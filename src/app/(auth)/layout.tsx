c8f144d2c1183c36af479700c4662fa6f030c172
Cynthia Chung <cynthiachung03@gmail.com>
2023-05-30 14:19:20 -0400

    Center sign-in and sign-up pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import {type PropsWithChildren} from "react";
+
+export default function AuthLayout({children}: PropsWithChildren) {
+    return <main className="al flex h-screen w-screen items-center justify-center">{children}</main>;
+}
